f54acd58f03a98f693247ce3dd95f95ecfb35d61
Halskov57 <nicolaihalskov@gmail.com>
2025-10-04 17:54:59 +0200

    changes imports

--- Backend/projectmanagement/src/main/java/bachelor/projectmanagement/graphql/ProjectResolver.java
+++ Backend/projectmanagement/src/main/java/bachelor/projectmanagement/graphql/ProjectResolver.java
@@ -1,11 +1,9 @@
 package bachelor.projectmanagement.graphql;
 
-import bachelor.projectmanagement.service.UserService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import bachelor.projectmanagement.model.User;
 import bachelor.projectmanagement.model.Project;
 import bachelor.projectmanagement.model.Epic;
 import bachelor.projectmanagement.model.Feature;
