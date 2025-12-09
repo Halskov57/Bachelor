6a420290fb8248d3c4583ec2d4da0bf8ac9c0c78
Jonas <202208584@post.au.dk>
2025-12-09 13:03:44 +0100

    minor graphql change

---
+++ Backend/projectmanagement/src/main/java/bachelor/projectmanagement/graphql/input/EpicInput.java
@@ -0,0 +1,25 @@
+package bachelor.projectmanagement.graphql.input;
+
+public class EpicInput {
+    private String title;
+    private String description;
+
+    public EpicInput() {}
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
